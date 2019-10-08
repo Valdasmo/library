5722e9bb98a97875d02aecdc555da810377b6522
Valdasmo <51168355+Valdasmo@users.noreply.github.com>
2019-10-08 18:05:02 +0300

    1-30

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['prefix' => 'authors'], function(){
+    Route::get('', 'AuthorController@index')->name('author.index');
+    Route::get('create', 'AuthorController@create')->name('author.create');
+    Route::post('store', 'AuthorController@store')->name('author.store');
+    Route::get('edit/{author}', 'AuthorController@edit')->name('author.edit');
+    Route::post('update/{author}', 'AuthorController@update')->name('author.update');
+    Route::post('delete/{author}', 'AuthorController@destroy')->name('author.destroy');
+    Route::get('show/{author}', 'AuthorController@show')->name('author.show');
+ });
+ 
+ 
